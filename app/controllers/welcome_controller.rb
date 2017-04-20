dade9659df18e975ffbd95294cbd96faff017d72
Ann Siapno <ann.n.siapno@gmail.com>
2017-04-20 22:09:37 +1000

    Error fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,22 +5,22 @@ class WelcomeController < ApplicationController
     @user_playlists = Playlist.where(user_id: @user.id)
   end
 
-<<<<<<< HEAD
   def search(input)
     @search = Song.search(params[:search])
     @resultsName = Song.where(title: @search)
     @resultsGenre = Song.where(genre: @search)
     @resultsArtist = Song.where(artist: @search)
     @resultsYear = Song.where(year: @search)
-=======
+  end
+
   def search()
     line = '--------------------------------------------'
-    @search = '%' + params[:search] + '%'
+    @search = params[:search] + '%'
     @resultsName = Song.where("title LIKE '#{@search}'")
     @resultsGenre = Song.where("genre LIKE '#{@search}'")
     @resultsArtist = Song.where("artist LIKE '#{@search}'")
     @resultsYear = Song.where("year LIKE '#{@search}'")
->>>>>>> 4df2179942b1010ed8b1542315f2ab464f3bf992
+
   end
 
 end
