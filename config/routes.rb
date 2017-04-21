eb531332cfa830246a711775d45751ba21961600
Ann Siapno <ann.n.siapno@gmail.com>
2017-04-21 10:31:36 +1000

    Create new playlist link and form

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/search'
 
+  resources :playlists
   resources :songs
   devise_for :users
- resources :users
- root 'users#index'
+  resources :users
+  root 'users#index'
 
 end
