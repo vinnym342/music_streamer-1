46e05523d1e36eff9a161dcc29b85b4527b89fde
Ann Siapno <ann.n.siapno@gmail.com>
2017-04-23 18:47:26 +1000

    Error fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
 
-    if Profile.exists?(:user_id => @user.id)
+    if Profile.exists?(:user_id => current_user.id)
       welcome_index_path
     else
       profile_create_page_path
