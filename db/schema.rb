379f4364efb0352b177df1a1b7f5e5d45f50d38d
Ann Siapno <ann.n.siapno@gmail.com>
2017-04-24 16:02:56 +1000

    Added Conversation and Message modules

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422152125) do
+ActiveRecord::Schema.define(version: 20170424055450) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["recipient_id"], name: "index_conversations_on_recipient_id"
+    t.index ["sender_id"], name: "index_conversations_on_sender_id"
+  end
 
   create_table "playlist_to_songs", force: :cascade do |t|
     t.integer  "playlist_id"
